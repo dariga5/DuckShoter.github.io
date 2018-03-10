345f2257c3d1714ed497ed271271878d18c78d1c
dariga5 <35176405+dariga5@users.noreply.github.com>
2018-03-10 22:48:57 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,39 @@
      var game = pjs.game;
 	 var key = pjs.keyControl;
 	 key.initKeyControl();
-	 var b = ""; 
-	 var p = pjs.vector.point;
+	 var b = "";
+   	 var  speed = 12;
 	
+	 var p = pjs.vector.point;
+	 var wall1 = game.newRectObject(   { 
+     x : 1, 
+     y : 1, 
+     w : 20, 
+     h : 650, 
+     fillColor : "red", 
+     });
+     var wall2 = game.newRectObject(   { 
+     x : 1, 
+     y : 1, 
+     w : 650, 
+     h : 20, 
+     fillColor : "red", 
+      });
+     var wall3 = game.newRectObject(   { 
+     x : 630, 
+     y : 1, 
+     w : 20, 
+     h : 650, 
+     fillColor : "red", 
+     });
+     var wall4 = game.newRectObject(   { 
+     x : 1, 
+     y : 460, 
+     w : 650, 
+     h : 20, 
+     fillColor : "red", 
+     });
+	 
 	 var AIM = game.newCircleObject(   { 
      x : 100, 
      y : 100, 
@@ -17,27 +47,87 @@
      alpha : 0.41, 
      visible : true 
    });
-	 
-	 
+    var WALL = [wall1,wall2,wall3,wall4];
+     
+	var management  = function (){	
+     dX = dY = 0;	
+	 if(key.isDown("D") || key.isDown("RIGHT")){
+		b="D or RIGHT";
+       	dX = speed	;		 
+	 }
+	 if(key.isDown("A") || key.isDown("LEFT")){
+		b="A or LEFT";
+       	dX = -speed	;		 
+	 } 
+	 if(key.isDown("W") || key.isDown("UP")){
+		b="W or UP";
+        dY = -speed	;			 
+	 }
+	 if(key.isDown("S") || key.isDown("DOWN")){
+		b="S or DOWN";
+        dY = speed	;	
+	 }
+	  }
+   var COLLISION = function(){
+		 
+     for(var i in WALL) {
+		 
+	  if(AIM.isStaticIntersect(WALL[i].getStaticBox())){
+		 
+		 if(dX == -speed){
+		 dX = 0;
+		 }
+			 
+	     if(dX == speed){
+		 dX = 0;
+		 }
+			 
+	     if(dY = -speed){
+		 dY = 0;
+		 }
+			 
+	     if(dY = -speed){
+		 dY = 0;
+		 }
+     }//if(AIM.isStaticIntersect( wall1.getStaticBox())){
+	 }  //for(var i in wall)  
+  
+	 }	//var COLLISION = function()   
+	  
+	
+	
 	 
 	 game.newLoop("myGame", function(){
+    	 
 	 
-	 function a1 (key1,key2,name,x,y){ 
-	 if(key.isDown(key1) || key.isDown(key2)){
-	 name.move(p(x,y) );
-	 b = key1 +" или "+ key2;
-	 }}
+	
+	
+	
+	
+	management();
+	COLLISION();
+	AIM.move(p(dX,dY));	
+	
+	
+	
+	
+	
+	
+	
+	AIM.draw();
+	wall1.draw();
+    wall2.draw();
+	wall3.draw();
+	wall4.draw();
+
+    AIM.drawStaticBox();
+    wall1.drawStaticBox();
+    wall2.drawStaticBox();
+    wall3.drawStaticBox();
+    wall4.drawStaticBox();
+   	
 	 
-	 a1("D" , "RIGHT" ,AIM,12,0 )
-	 a1("A" , "LEFT" ,AIM,-12,0 )
-	 a1("S" , "DOWN" ,AIM,0,12 )
-	 a1("W" , "UP" ,AIM,0,-12 )
-	 
-	 
-	 
-	 
-	 
-	 AIM.draw();
+ 	 
 	 
 	 
 	 
@@ -56,4 +146,3 @@
 	 
 	game.setLoop("myGame");
 	game.start();
-	
